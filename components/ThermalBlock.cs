6cae06c81a33a7ac8d95d8efcbfe1a5a801f3f6f
saitamasaitama <s-yoshihara@saitama-tec.co.jp>
2019-12-31 13:30:40 +0900

    the first

---
+++ components/ThermalBlock.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ThermalBlock : MonoBehaviour
+{
+  private List<ThermalBlock> contacts=new List<ThermalBlock>();
+
+
+  //比熱係数 
+  public float C=3f;
+  //現在与えられているカロリー
+  public double calorie=0;
+  //質量(g)
+  public float mass=10*10*10;
+  
+  //熱伝導率(1mあたり W)
+  public float thermalConductivity = 1000;
+
+//1w＝1/（4.184×時間［s］）cal
+//1cal＝（4.184×時間［s］）w
+  
+  public static float Watt2cal(float w,float delta)=>w*delta*4.184f;
+
+  public float Kelvin{
+    set{
+      calorie= value*C*mass;
+    }
+    get=>(float)calorie/(mass*C);
+  }
+  private Material mat;
+
+    void Start()
+    {
+      this.mat= this.GetComponent<Renderer>().material;
+    }
+
+    public void Update(){
+      mat.color=Color.HSVToRGB(
+        0,
+        1f,
+        (Kelvin/1000f)
+      );
+    }
+
+
+
+    public void FixedUpdate(){
+      float delta= Time.fixedDeltaTime;
+      foreach(ThermalBlock b in contacts){
+        UpdateThermal(delta,this,b);
+      }
+    }
+
+    //接触している面積：時間：熱伝導率により相手に熱を与える
+    private static void UpdateThermal(float delta,ThermalBlock A,ThermalBlock B){
+      //隣の熱と自分の熱を交換
+      
+      //熱伝導率を 1K = 1Secとする
+      if(A.Kelvin < B.Kelvin){
+        float cal= Watt2cal(A.thermalConductivity,delta);  
+        //Aに対して熱が送られる
+        A.calorie += cal;
+        B.calorie -= cal;        
+      }
+
+      else if(B.Kelvin < A.Kelvin){
+        float cal= Watt2cal(B.thermalConductivity,delta);  
+        //Bに対して熱が送られる
+        B.calorie += cal;
+        A.calorie -= cal;        
+      }
+
+
+    }
+
+    void OnCollisionEnter(Collision other){
+      var o= other.gameObject.GetComponent<ThermalBlock>();
+
+      if(o!=null){
+        contacts.Add(o);
+      }
+    }
+
+    void OnCollisionExit(Collision other){
+
+      var o= other.gameObject.GetComponent<ThermalBlock>();
+
+      if(o!=null){
+        contacts.Remove(o);
+      }
+
+    }
+
+    public static ThermalBlock operator + (ThermalBlock A,ThermalBlock B){
+      
+
+      return A;
+    }
+
+}
---
+++ components/ThermalMachine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ThermalMachine : MonoBehaviour
+{
+
+    public void xyWalk(int w, int h, Action<int, int> a)
+    {
+        for (int x = 0; x < w; x++)
+            for (int y = 0; y < h; y++)
+                a(x, y);
+    }
+    void Start()
+    {
+        xyWalk(10,10, (y, x) =>
+        {
+            GameObject o = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            o.transform.position = 
+                (Vector3.up * (y + 1) * 1.5f) + 
+                (Vector3.left * (x -5)
+            );
+            Rigidbody rigidbody= o.AddComponent<Rigidbody>();
+
+            rigidbody.constraints = 
+                RigidbodyConstraints.FreezePositionX
+                |RigidbodyConstraints.FreezePositionZ
+                |RigidbodyConstraints.FreezeRotation
+                ;
+            o.AddComponent<BoxCollider>();
+            o.GetComponent<Renderer>().material = new Material(Shader.Find("Unlit/Color"));
+
+            ThermalBlock thermal=o.AddComponent<ThermalBlock>();
+            thermal.Kelvin=UnityEngine.Random.Range(0,1000);
+
+            
+
+        });
+    }
+}
